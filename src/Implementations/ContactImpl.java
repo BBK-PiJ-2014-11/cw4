bee4adb39fceaeb7067a6e66ee861e54b1902240
Ehshan <eveerabangsa@gmail.com>
2015-02-18 22:39:36 +0000

    added class constructor

--- src/Implementations/ContactImpl.java
+++ src/Implementations/ContactImpl.java
@@ -3,6 +3,18 @@ package Implementations;
 import Interfaces.Contact;
 
 public class ContactImpl implements Contact {
+
+    private int id;
+    private String name;
+    private String notes;
+
+    public ContactImpl(int id, String name, String notes ) {
+        this.id = id;
+        this.name = name;
+        this.notes = notes;
+    }
+
+
     @Override
     public int getId() {
         return 0;
