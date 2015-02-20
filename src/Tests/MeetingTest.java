233498c26feea325305706d0bda213119700f581
Ehshan <eveerabangsa@gmail.com>
2015-02-20 16:45:19 +0000

    created MeetingTest with outline of tests

---
+++ src/Tests/MeetingTest.java
@@ -0,0 +1,35 @@
+package Tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MeetingTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void testGetId() {
+
+    }
+
+    @Test
+    public void testGetDate() {
+
+    }
+
+    @Test
+    public void testGetContacts() {
+
+    }
+}
