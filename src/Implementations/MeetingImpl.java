29ca97e48cb8052f69d7f70cec703c740fe742f9
Ehshan <eveerabangsa@gmail.com>
2015-02-22 23:28:12 +0000

    added JavaDoc

--- src/Implementations/MeetingImpl.java
+++ src/Implementations/MeetingImpl.java
@@ -5,29 +5,47 @@ import Interfaces.Meeting;
 
 import java.util.Calendar;
 import java.util.Set;
-
+/**
+ * {@inheritDoc}
+ *
+ * @author Ehshan Veerabangsa
+ */
 public class MeetingImpl implements Meeting {
 
     private int id;
     private Calendar date;
     private Set<Contact> contacts;
-
+    /**
+     * MeetingImpl class constructor
+     *
+     * Creates a new meeting
+     *
+     * @param id the meeting's (unique) id number
+     * @param date the date of the meeting
+     * @param contacts the set of contacts attending the meeting
+     */
     public MeetingImpl(int id, Calendar date, Set<Contact>contacts ) {
         this.id = id;
         this.date = date;
         this.contacts = contacts;
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getId() {
         return id;
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Calendar getDate() {
         return date;
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Set<Contact> getContacts() {
         return contacts;
