9f10569766d42ba8d1f9ec9b5beaece9b1ae267d
Ehshan <eveerabangsa@gmail.com>
2015-02-23 22:38:48 +0000

    created PastMeetingTest with outline of tests

---
+++ src/Tests/PastMeetingTest.java
@@ -0,0 +1,25 @@
+package Tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PastMeetingTest {
+
+    @Before
+    public void setUp(){
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void testGetNotes() {
+
+    }
+}
