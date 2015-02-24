97e665faddf7b810be8e5796633d8039dd0b7fa1
Ehshan <eveerabangsa@gmail.com>
2015-02-24 22:35:55 +0000

    added JavaDoc annotation

--- src/Implementations/PastMeetingImpl.java
+++ src/Implementations/PastMeetingImpl.java
@@ -5,17 +5,32 @@ import Interfaces.PastMeeting;
 
 import java.util.Calendar;
 import java.util.Set;
-
+/**
+ * {@inheritDoc}
+ *
+ * @author Ehshan Veerabangsa
+ */
 public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
 
     private String notes;
-
+    /**
+     * PastMeetingImpl class constructor
+     *
+     * Creates a new past meeting
+     *
+     * @param id the meeting's (unique) id number
+     * @param date the date of the meeting
+     * @param contacts the set of contacts attending the meeting
+     * @param notes notes about what happened and what was agreed
+     */
     // n.b. need to review whether notes should be used as a parameter - or possibly use multiple constructors
     public PastMeetingImpl(int id, Calendar date,Set<Contact> contacts, String notes) {
         super(id,date,contacts);
         this.notes = notes;
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getNotes() {
         return notes;
