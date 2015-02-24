44b6c69dd9525a54c15540af62d3a12c02369144
Ehshan <eveerabangsa@gmail.com>
2015-02-24 21:57:29 +0000

    added extends keyword and removed inherited methods

--- src/Implementations/PastMeetingImpl.java
+++ src/Implementations/PastMeetingImpl.java
@@ -6,24 +6,10 @@ import Interfaces.PastMeeting;
 import java.util.Calendar;
 import java.util.Set;
 
-public class PastMeetingImpl implements PastMeeting {
+public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
     @Override
     public String getNotes() {
         return null;
     }
 
-    @Override
-    public int getId() {
-        return 0;
-    }
-
-    @Override
-    public Calendar getDate() {
-        return null;
-    }
-
-    @Override
-    public Set<Contact> getContacts() {
-        return null;
-    }
 }
