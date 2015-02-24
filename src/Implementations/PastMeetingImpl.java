b88b7e00787332cfff151a24f706c589970dfced
Ehshan <eveerabangsa@gmail.com>
2015-02-24 21:06:25 +0000

    outline implementation of PastMeeting. Complies-all methods return null

---
+++ src/Implementations/PastMeetingImpl.java
@@ -0,0 +1,29 @@
+package Implementations;
+
+import Interfaces.Contact;
+import Interfaces.PastMeeting;
+
+import java.util.Calendar;
+import java.util.Set;
+
+public class PastMeetingImpl implements PastMeeting {
+    @Override
+    public String getNotes() {
+        return null;
+    }
+
+    @Override
+    public int getId() {
+        return 0;
+    }
+
+    @Override
+    public Calendar getDate() {
+        return null;
+    }
+
+    @Override
+    public Set<Contact> getContacts() {
+        return null;
+    }
+}
