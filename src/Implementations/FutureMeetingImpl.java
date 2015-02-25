48c687bb24314ac41402699ec17e5c3c684efe45
Ehshan <eveerabangsa@gmail.com>
2015-02-25 21:59:11 +0000

    added extends keyword and removed inherited methods

--- src/Implementations/FutureMeetingImpl.java
+++ src/Implementations/FutureMeetingImpl.java
@@ -6,19 +6,6 @@ import Interfaces.FutureMeeting;
 import java.util.Calendar;
 import java.util.Set;
 
-public class FutureMeetingImpl implements FutureMeeting {
-    @Override
-    public int getId() {
-        return 0;
-    }
+public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting {
 
-    @Override
-    public Calendar getDate() {
-        return null;
-    }
-
-    @Override
-    public Set<Contact> getContacts() {
-        return null;
-    }
 }
