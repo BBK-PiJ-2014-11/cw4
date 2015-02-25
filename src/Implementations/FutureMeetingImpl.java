e39f9f6809679dea75e1619c99e088491485078d
Ehshan <eveerabangsa@gmail.com>
2015-02-25 21:57:00 +0000

    outline implementation of FutureMeeting. Complies-all methods return null

---
+++ src/Implementations/FutureMeetingImpl.java
@@ -0,0 +1,24 @@
+package Implementations;
+
+import Interfaces.Contact;
+import Interfaces.FutureMeeting;
+
+import java.util.Calendar;
+import java.util.Set;
+
+public class FutureMeetingImpl implements FutureMeeting {
+    @Override
+    public int getId() {
+        return 0;
+    }
+
+    @Override
+    public Calendar getDate() {
+        return null;
+    }
+
+    @Override
+    public Set<Contact> getContacts() {
+        return null;
+    }
+}
