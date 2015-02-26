7bb5b3d5eef698c3584dba74e356dd84d8148843
Ehshan <eveerabangsa@gmail.com>
2015-02-26 22:27:31 +0000

    added ContactManagerTest

---
+++ src/Tests/ContactManagerTest.java
@@ -0,0 +1,20 @@
+package Tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ContactManagerTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+}
