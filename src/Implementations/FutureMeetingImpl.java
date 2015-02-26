c39b7a65e4607c5ab75679e55cbb00e65eaa309f
Ehshan <eveerabangsa@gmail.com>
2015-02-26 21:50:41 +0000

    added JavaDoc annotations

--- src/Implementations/FutureMeetingImpl.java
+++ src/Implementations/FutureMeetingImpl.java
@@ -5,9 +5,21 @@ import Interfaces.FutureMeeting;
 
 import java.util.Calendar;
 import java.util.Set;
-
+/**
+ * {@inheritDoc}
+ *
+ * @author Ehshan Veerabangsa
+ */
 public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting {
-
+    /**
+     * FutureMeetingImpl class constructor
+     *
+     * Creates a new future meeting
+     *
+     * @param id the meeting's (unique) id number
+     * @param date the date of the meeting
+     * @param contacts the set of contacts attending the meeting
+     */
     public FutureMeetingImpl(int id, Calendar date,Set<Contact> contacts) {
         super(id,date,contacts);
     }
