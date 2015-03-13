6e08a37521dc36c5b7fbb9ed0c02bdefe50d8e9d
Ehshan <eveerabangsa@gmail.com>
2015-03-13 22:36:50 +0000

    updated interface for XMLHandler

--- src/Interfaces/XMLHandler.java
+++ src/Interfaces/XMLHandler.java
@@ -1,6 +1,10 @@
 package Interfaces;
 
 import java.io.File;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Set;
+
 /**
  * A class read and write data to and from an XML file
  */
@@ -18,4 +22,14 @@ public interface XMLHandler {
      * @param file the file to be read and parsed
      */
     public void parseData(File file);
+    /**
+     * Creates a Document Object from contact manager data
+     */
+    public void createDocument(Set<Contact> contacts, List<Meeting> meetings, Calendar today, int currentContactId, int currentMeetingId);
+    /**
+     * Writes a Document Object to an XML file
+     *
+     * @param file the file to be written to
+     */
+    public void writeFile(String file);
 }
