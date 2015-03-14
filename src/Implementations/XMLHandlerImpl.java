75481d911dcb5fda5e6b6e5b718142ddf43346b6
Ehshan <eveerabangsa@gmail.com>
2015-03-14 20:41:48 +0000

    outline implementation of XMLHandler

---
+++ src/Implementations/XMLHandlerImpl.java
@@ -0,0 +1,31 @@
+package Implementations;
+
+import Interfaces.Contact;
+import Interfaces.Meeting;
+import Interfaces.XMLHandler;
+
+import java.io.File;
+import java.util.List;
+import java.util.Set;
+
+public class XMLHandlerImpl implements XMLHandler {
+    @Override
+    public void readFile() {
+
+    }
+
+    @Override
+    public void parseData(File file) {
+
+    }
+
+    @Override
+    public void createDocument(Set<Contact> contacts, List<Meeting> meetings, int currentContactId, int currentMeetingId) {
+
+    }
+
+    @Override
+    public void writeFile(File file) {
+
+    }
+}
