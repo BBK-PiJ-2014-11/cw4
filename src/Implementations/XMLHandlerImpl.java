84cd5b9cedc7c18193ae722bb0baa3c61a5b4b47
Ehshan <eveerabangsa@gmail.com>
2015-03-14 20:48:05 +0000

    amended JavaDoc

--- src/Implementations/XMLHandlerImpl.java
+++ src/Implementations/XMLHandlerImpl.java
@@ -7,23 +7,36 @@ import Interfaces.XMLHandler;
 import java.io.File;
 import java.util.List;
 import java.util.Set;
-
+/**
+ * {@inheritDoc}
+ *
+ * @author Ehshan Veerabangsa
+ */
 public class XMLHandlerImpl implements XMLHandler {
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void readFile() {
 
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void parseData(File file) {
 
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void createDocument(Set<Contact> contacts, List<Meeting> meetings, int currentContactId, int currentMeetingId) {
 
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void writeFile(File file) {
 
